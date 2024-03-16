cc5ecd7b45a2095e483dacb80516fc571531e482
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-03-16 22:49:27 +0330

    refactor: user model synced with DB schema

--- backend/internal/core/models.go
+++ backend/internal/core/models.go
@@ -1,10 +1,13 @@
 package core
 
 type User struct {
-	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Salt     string `json:"salt"`
+	ID       int     `json:"id"`
+	Email    string  `json:"email"`
+	Password string  `json:"password"`
+	Salt     string  `json:"salt"`
+	Name     string  `json:"name"`
+	Type     string  `json:"type"`
+	Score    float32 `json:"score"`
 }
 
 type Session struct {
