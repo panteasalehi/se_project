66d090098d3dc7261546aaacd3cae6d343c176ef
panteasalehi <pantolia.salehi@gmail.com>
2024-05-05 21:56:15 +0330

    map added

--- src/Makepost.jsx
+++ src/Makepost.jsx
@@ -1,5 +1,40 @@
-import React from "react"
+import React, { useState } from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '50vw',
+  height: '50vh',
+};
+const center = {
+  lat: 35.7219, // default latitude
+  lng: 51.3347, // default longitude
+};
+
+
 export const Makepost = () => {
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyCNVtpIqQ_AMO-Ov07BInNpgkuinI3Zr6Y',
+        libraries,
+      });
+    
+      const [markerPosition, setMarkerPosition] = useState(null);
+    
+      const handleMapClick = (event) => {
+        setMarkerPosition({
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng(),
+        });
+      };
+    
+      if (loadError) {
+        return <div>Error loading maps</div>;
+      }
+    
+      if (!isLoaded) {
+        return <div>Loading maps</div>;
+      }
+      
     return (
         <div class="wrapper">
         <div class="header">logo will be here</div>
@@ -52,18 +87,21 @@ export const Makepost = () => {
         <br></br>
         </div>
 
-        <div className = "map">
-
         </div>
-
-        <div className = "post-cancle">
-
+        <div className="four">
+            
+        <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+        onClick={handleMapClick}
+      >
+        {markerPosition && <Marker position={markerPosition} />}
+      </GoogleMap>
         </div>
-
         </div>
 
         
-        </div>
     )
   }
   
