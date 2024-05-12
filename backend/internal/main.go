1cfe6ff30743af04ab5249f50238832129ac75a6
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-05-12 19:40:22 +0330

    feat: chat endpoints added to handler

--- backend/internal/main.go
+++ backend/internal/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"MelkOnline/internal/controller/ADregister"
 	"MelkOnline/internal/controller/auth"
+	"MelkOnline/internal/controller/chat"
 	"MelkOnline/internal/controller/signup"
 
 	echo "github.com/labstack/echo/v4"
@@ -13,5 +14,7 @@ func main() {
 	e.POST("/signup", signup.NewSignupHandler().Signup)
 	e.POST("/login", auth.NewLoginHandler().Login)
 	e.POST("/ADregister", ADregister.NewADregisterHandler().ADregister)
+	e.GET("/Chat/page/?chatid=", chat.NewChatHandler().GetMessagesByChatID)
+	e.POST("/Chat/send/?chatid=", chat.NewChatHandler().SendMessage)
 	e.Logger.Fatal(e.Start(":8086"))
 }
