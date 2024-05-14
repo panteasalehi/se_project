5f69d3ef15ebbf25c0ba08424d7c2315df2c6143
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-05-15 03:20:45 +0330

    fix: api now serves frontend pages to user

--- backend/exec/main.go
+++ backend/exec/main.go
@@ -11,10 +11,18 @@ import (
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+
+	e.Static("/", "public")
+	e.File("/", "public/index.html")
+
+
 	e.POST("/signup", signup.NewSignupHandler().Signup)
 	e.POST("/login", auth.NewLoginHandler().Login)
 	e.POST("/ADregister", ADregister.NewADregisterHandler().ADregister)
 	e.GET("/Chat/page/?chatid=", chat.NewChatHandler().GetMessagesByChatID)
 	e.POST("/Chat/send/?chatid=", chat.NewChatHandler().SendMessage)
-	e.Logger.Fatal(e.Start(":8086"))
+	
+	e.Logger.Fatal(e.Start(":8080"))
 }
