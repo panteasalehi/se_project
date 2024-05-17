e3d714eac44fcdc91cdffa5a1e60672312dbaeb2
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-05-17 17:32:24 +0330

    test: signup service tested

---
+++ backend/test/core/signup_test.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	model "MelkOnline/internal/core"
+	"MelkOnline/internal/core/signup"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_signup_service(t *testing.T) {
+	err := godotenv.Load("/home/ssaeidifarzad/ssfdata/ssaeidifarzad/Classes/S8/SE/Project/SE_project/backend/.env")
+	if err != nil {
+		panic(err)
+	}
+	user := &model.User{
+		Email:    "abcd",
+		Password: "abcd",
+		Name:     "abcd",
+	}
+	sr := signup.NewSignupService()
+	ID, err := sr.Signup(user.Email, user.Password, user.Name)
+	assert.Nil(t, err, "Error should be nil")
+	assert.NotEqual(t, 0, ID, "ID should not be 0")
+}
