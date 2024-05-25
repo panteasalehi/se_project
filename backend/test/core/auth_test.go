58ed086e9db42577e9d72d02015dc054166d0a94
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-05-25 14:11:43 +0330

    auth core test fixed

--- backend/test/core/auth_test.go
+++ backend/test/core/auth_test.go
@@ -3,6 +3,7 @@ package core
 import (
 	model "MelkOnline/internal/core"
 	"MelkOnline/internal/core/auth"
+	"MelkOnline/internal/infrastructure/signup"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,10 +11,12 @@ import (
 
 func Test_auth_valid_core(t *testing.T) {
 	user := &model.User{
-		Email:    "galskjd",
+		Email:    "test@example.com",
 		Password: "abcd",
 		Name:     "abcd",
 	}
+	sr := signup.NewSignupRepository()
+	sr.StoreUser(user.Email, user.Password, user.Name, auth.NewAuthService().GenerateToken(), "user", 0)
 	as := auth.NewAuthService()
 	session, err := as.Login(user.Email, user.Password)
 	assert.Nil(t, err, "Error should be nil")
@@ -22,7 +25,7 @@ func Test_auth_valid_core(t *testing.T) {
 
 func Test_auth_invalid_core(t *testing.T) {
 	user := &model.User{
-		Email:    "galskjd",
+		Email:    "test@example.com",
 		Password: "wrong",
 		Name:     "abcd",
 	}
