fceba89d50186c7f94348cad71f1f2c02e9db0c4
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-07-02 13:36:42 +0330

    refactor: signup checks email and password by regex

--- backend/internal/controller/signup/signup_handler.go
+++ backend/internal/controller/signup/signup_handler.go
@@ -4,6 +4,7 @@ import (
 	"MelkOnline/internal/controller"
 	"MelkOnline/internal/core/signup"
 	"net/http"
+	"regexp"
 
 	echo "github.com/labstack/echo/v4"
 )
@@ -31,12 +32,16 @@ func NewSignupHandler() *SignupHandler {
 //	@Success		200			{string}	string
 //	@Failure		400			{string}	string
 //	@Failure		500			{string}	string
-//	@Router			/signup/ [post]
+//	@Router			/api/v1/signup/ [post]
 func (sh *SignupHandler) Signup(c echo.Context) error {
 	sreq := &controller.SignupRequest{}
 	sres := &controller.SignupResponse{}
 	if err := c.Bind(sreq); err != nil {
 		sres.Message = err.Error()
+		return c.JSON(http.StatusBadRequest, sres)
+	}
+	if !isValidEmail(sreq.Email) || !isValidPassword(sreq.Password) {
+		sres.Message = "Invalid email or password"
 		return c.JSON(http.StatusBadRequest, sres)
 	}
 	ID, token, err := sh.ss.Signup(sreq.Email, sreq.Password, sreq.Name)
@@ -55,4 +60,25 @@ func (sh *SignupHandler) Signup(c echo.Context) error {
 	cookie.SameSite = http.SameSiteLaxMode
 	c.SetCookie(cookie)
 	return c.JSON(http.StatusOK, sres)
+}
+
+func isValidPassword(password string) bool {
+	var (
+		hasMinLen  = regexp.MustCompile(`.{8,}`)
+		hasUpper   = regexp.MustCompile(`[A-Z]`)
+		hasLower   = regexp.MustCompile(`[a-z]`)
+		hasNumber  = regexp.MustCompile(`\d`)
+		hasSpecial = regexp.MustCompile(`[!@#$%^&*()]`)
+	)
+
+	return hasMinLen.MatchString(password) &&
+		hasUpper.MatchString(password) &&
+		hasLower.MatchString(password) &&
+		hasNumber.MatchString(password) &&
+		hasSpecial.MatchString(password)
+}
+
+func isValidEmail(email string) bool {
+	var emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return emailRegex.MatchString(email)
 }
