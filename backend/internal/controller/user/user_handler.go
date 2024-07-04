fddd880ace6abc6642b8b99f67c4a440914d051e
Sepehr Saeidi Farzad <ssaeidifarzad@gmail.com>
2024-07-04 09:13:27 +0330

    feat: user handler added

---
+++ backend/internal/controller/user/user_handler.go
@@ -0,0 +1,29 @@
+package user
+
+import (
+	"MelkOnline/internal/core/user"
+
+	"github.com/labstack/echo/v4"
+)
+
+type UserHandler struct {
+	us user.UserServiceContract
+}
+
+func NewUserHandler() *UserHandler {
+	return &UserHandler{
+		us: user.NewUserService(),
+	}
+}
+
+func (uh *UserHandler) GetUserBySession(c echo.Context) error {
+	token, err := c.Request().Cookie("token")
+	if err != nil {
+		return c.JSON(400, err.Error())
+	}
+	user, err := uh.us.GetUserBySession(token.Value)
+	if err != nil {
+		return c.JSON(400, err.Error())
+	}
+	return c.JSON(200, user)
+}
